136694d272dff678a7d5d028851b323cb2c87f58
sonhaichu <sonhaichu@gmailcom>
2018-12-06 14:04:28 +0700

    update password and avatar apis

--- src/app/Validators/AuthValidator.php
+++ src/app/Validators/AuthValidator.php
@@ -8,13 +8,21 @@ use VCComponent\Laravel\User\Validators\AbstractValidator;
 class AuthValidator extends AbstractValidator
 {
     protected $rules = [
-        'LOGIN'        => [
+        'LOGIN'                => [
             'email'    => ['required', 'email'],
             'password' => ['required', 'min:6'],
         ],
-        'SOCIAL_LOGIN' => [
+        'SOCIAL_LOGIN'         => [
             'provider'     => ['required'],
             'access_token' => ['required'],
+        ],
+        'RULE_UPDATE_AVATAR'   => [
+            'avatar' => ['required'],
+        ],
+        'RULE_UPDATE_PASSWORD' => [
+            'old_password'              => ['required'],
+            'new_password'              => ['required', 'min:6', 'max:30'],
+            'new_password_confirmation' => ['required'],
         ],
     ];
 }
